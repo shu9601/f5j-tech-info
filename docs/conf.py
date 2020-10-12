d0242d905529758e09baa8161fa0a8322b28ef3c
Motoko TANAKA <M.TANAKA@f5.com>
2020-10-12 12:56:07 +0900

    タイポ修正 F5ネットワーク→F5ネットワークス

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = u'F5ネットワークジャパン テクニカルインフォ'
+project = u'F5ネットワークスジャパン テクニカルインフォ'
 copyright = u'F5ネットワークスジャパン合同会社'
 author = u'F5ネットワークスジャパン合同会社'
 
